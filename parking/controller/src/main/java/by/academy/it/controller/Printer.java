8632573cad57afe9d65cfc27141fe222d004992d
davydovbv <bogedavydov@gmail.com>
2021-10-01 16:30:48 +0300

    fix: remove unnecessary symbols

--- parking/controller/src/main/java/by/academy/it/controller/Printer.java
+++ parking/controller/src/main/java/by/academy/it/controller/Printer.java
@@ -4,10 +4,8 @@ import by.academy.it.data.Ticket;
 
 public class Printer {
 
-    public Ticket printTicket(String licenseNumber,
-                            String date) {
+    public Ticket printTicket(String licenseNumber, String date) {
         Ticket ticket = new Ticket();
         return ticket;
     }
-
 }
--- parking/data/src/main/java/by/academy/it/data/Ticket.java
+++ parking/data/src/main/java/by/academy/it/data/Ticket.java
@@ -3,8 +3,6 @@ package by.academy.it.data;
 import java.util.Date;
 
 public class Ticket {
-
     private String licensePlateNumber;
     private Date date;
-
 }
