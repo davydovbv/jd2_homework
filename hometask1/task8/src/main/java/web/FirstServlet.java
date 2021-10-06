4604ab3d001fc097871ee099787d5c16b906b88b
davydovbv <bogedavydov@gmail.com>
2021-10-06 19:24:08 +0300

    feat: add FirstServlet class

---
+++ hometask1/task8/src/main/java/web/FirstServlet.java
@@ -0,0 +1,26 @@
+package web;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "firstServlet", urlPatterns = "/first")
+public class FirstServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.println("<html><head><title>First Servlet</title></head>");
+        out.println("<body><h1>This is First Servlet</h1>");
+        out.println("</body></html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req,resp);
+    }
+}
